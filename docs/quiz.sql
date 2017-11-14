a1cd7f9d3c56a6dc7dc59ab73969b7974e2367b8
quangnv <quangnv@git.ominext.com>
2017-11-14 23:33:26 +0700

    system

--- docs/quiz.sql
+++ docs/quiz.sql
@@ -41,7 +41,7 @@ CREATE INDEX idx_student_information ON "public".student_information ( i_faculty
 CREATE TABLE "public".subject_information ( 
 	i_subject_information_pk integer  NOT NULL,
 	str_subject_information_name varchar(100)  NOT NULL,
-	i_subject_information_credit_num integer  NOT NULL,
+	i_subject_information_credits_num integer  NOT NULL,
 	i_subject_information_pk_ek integer  ,
 	CONSTRAINT pk_subjects_information PRIMARY KEY ( i_subject_information_pk )
  );
@@ -61,66 +61,65 @@ CREATE INDEX idx_teacher_information ON "public".teacher_information ( i_faculty
 
 CREATE TABLE "public".test_information ( 
 	i_test_information_pk integer  NOT NULL,
-	i_test_information_time integer  NOT NULL,
-	i_rate_of_difficulty_pk integer  ,
+	i_student_information_pk integer  ,
+	f_examination_result_score float8  ,
+	i_examination_result_num_answer_true integer  ,
+
 	i_test_information_pk_ek integer  ,
 	CONSTRAINT pk_test_information PRIMARY KEY ( i_test_information_pk )
  );
 
-CREATE INDEX idx_test_information ON "public".test_information ( i_rate_of_difficulty_pk );
+ALTER TABLE "public".test_information ADD COLUMN i_examination_information_pk integer NOT NULL ;
 
-CREATE TABLE "public".examination_list ( 
-	i_examination_list_pk integer  NOT NULL,
+CREATE TABLE "public".examination_information ( 
+	i_examination_information_pk integer  NOT NULL,
+	str_examination_information_code varchar(50) UNIQUE NOT NULL,
 	i_examination_room_pk integer  ,
 	i_subject_information_pk integer  ,
 	i_teacher_information_pk integer  ,
-	dt_examination_day   date  ,
-	i_examination_list_pk_ek integer  ,
-	CONSTRAINT pk_examination_list PRIMARY KEY ( i_examination_list_pk )
+	dt_examination_day   timestamp   ,
+
+	i_rate_of_difficulty_pk integer  ,      /*danh gia do kho cua bai thi*/
+	i_test_information_time integer  NOT NULL,    /*thoi gian lam bai thi*/
+
+	i_examination_information_pk_ek integer  ,
+	CONSTRAINT pk_examination_information PRIMARY KEY ( i_examination_information_pk )
  );
 
-CREATE INDEX idx_examination_list ON "public".examination_list ( i_examination_room_pk );
+CREATE INDEX idx_examination_information3 ON "public".examination_information ( i_rate_of_difficulty_pk );
 
-CREATE INDEX idx_examination_list_0 ON "public".examination_list ( i_subject_information_pk );
+CREATE INDEX idx_examination_information_2 ON "public".examination_information (str_examination_information_code);
 
-CREATE INDEX idx_examination_list_1 ON "public".examination_list ( i_teacher_information_pk );
+CREATE INDEX idx_examination_information ON "public".examination_information ( i_examination_room_pk );
 
-CREATE TABLE "public".examination_list_detail ( 
-	i_examination_list_detail_pk integer  NOT NULL,
-	i_examination_list_pk integer  ,
-	i_examination_list_detail_pk_ek integer  ,
-	CONSTRAINT pk_examination_list_detail PRIMARY KEY ( i_examination_list_detail_pk )
- );
-ALTER TABLE "public".examination_list_detail ADD COLUMN i_student_information_pk integer;
-ALTER TABLE "public".examination_list_detail ADD CONSTRAINT fk_examination_list_detail_to_student_information FOREIGN KEY ( i_student_information_pk ) REFERENCES "public".student_information( i_student_information_pk );
-CREATE INDEX idx_examination_list_detail ON "public".examination_list_detail ( i_examination_list_pk );
+CREATE INDEX idx_examination_information_0 ON "public".examination_information ( i_subject_information_pk );
 
-CREATE TABLE "public".examination_result ( 
-	i_examination_result_pk integer  NOT NULL,
-	i_test_information_pk integer  ,
-	i_student_information_pk integer  ,
-	f_examination_result_score float8  ,
-	i_examination_result_num_answer_true integer  ,
-	i_examination_result_pk_ek integer  ,
-	CONSTRAINT pk_examination_result PRIMARY KEY ( i_examination_result_pk )
+CREATE INDEX idx_examination_information_1 ON "public".examination_information ( i_teacher_information_pk );
+
+CREATE TABLE "public".examination_information_detail ( 
+	i_examination_information_detail_pk integer  NOT NULL,
+	
+	i_examination_information_pk integer  ,
+	i_student_information_pk integer ,
+
+	i_examination_information_detail_pk_ek integer  ,
+	CONSTRAINT pk_examination_information_detail PRIMARY KEY ( i_examination_information_detail_pk )
  );
 
-CREATE INDEX idx_examination_result ON "public".examination_result ( i_student_information_pk );
-
-CREATE INDEX idx_examination_result_0 ON "public".examination_result ( i_test_information_pk );
+CREATE INDEX idx_examination_information_detail ON "public".examination_information_detail ( i_examination_information_pk );
 
 CREATE TABLE "public".question_information ( 
 	i_question_information_pk integer  NOT NULL,
 	i_teacher_information_pk integer  ,
 	i_subject_information_pk integer  ,
-	i_question_level_information integer  ,
+	i_question_information_level integer  ,
 	str_question_content_information varchar(1000)  NOT NULL,
 	str_answer_1         varchar(500)  ,
 	str_answer_2         varchar(500)  ,
 	str_answer_3         varchar(500)  ,
 	str_answer_4         varchar(500)  ,
 	str_answer_5         varchar(500)  ,
-	str_answer_true      varchar(500)  NOT NULL,
+	str_true_answer      varchar(500)  NOT NULL,
 	i_question_information_pk_ek integer  ,
 	CONSTRAINT pk_question_information PRIMARY KEY ( i_question_information_pk )
  );
@@ -129,16 +128,19 @@ CREATE INDEX idx_question_information ON "public".question_information ( i_teach
 
 CREATE INDEX idx_question_information_subject ON "public".question_information ( i_subject_information_pk );
 
-CREATE INDEX idx_question_information_level ON "public".question_information ( i_question_level_information );
+CREATE INDEX idx_question_information_level ON "public".question_information ( i_question_information_level );
 
-CREATE TABLE "public".test_detail_information ( 
-	i_test_detail_information_pk integer  NOT NULL,
+CREATE TABLE "public".test_information_detail ( 
+	i_test_information_detail_pk integer  NOT NULL,
 	i_test_information_pk integer  ,
 	i_question_information_pk integer  ,
+	str_student_answer varchar(500) ,
 	i_test_detail_information_pk_ek integer  ,
-	CONSTRAINT pk_test_detail_information PRIMARY KEY ( i_test_detail_information_pk )
+	CONSTRAINT pk_test_information_detail PRIMARY KEY ( i_test_information_detail_pk )
  );
- CREATE TABLE "public".seiUser ( 
+
+
+CREATE TABLE "public".seiUser ( 
 	user_id varchar(50) NOT NULL ,
 	"password" varchar(100) NOT NULL,
 	top_menu varchar(5) default 'ADM' ,
@@ -154,21 +156,21 @@ REFERENCES "public".student_information( i_student_information_pk );
 ALTER TABLE "public".seiUser ADD CONSTRAINT fk_seiUser_to_teacher FOREIGN KEY ( i_teacher_information_pk ) 
 REFERENCES "public".teacher_information( i_teacher_information_pk );
 
-CREATE INDEX idx_test_detail_information_question ON "public".test_detail_information ( i_question_information_pk );
+CREATE INDEX idx_test_detail_information_question ON "public".test_information_detail ( i_question_information_pk );
 
-CREATE INDEX idx_test_detail_information_to_test ON "public".test_detail_information ( i_test_information_pk );
+CREATE INDEX idx_test_detail_information_to_test ON "public".test_information_detail ( i_test_information_pk );
 
-ALTER TABLE "public".examination_list ADD CONSTRAINT fk_examination_list_to_examination_room FOREIGN KEY ( i_examination_room_pk ) REFERENCES "public".examination_room( i_examination_room_pk ) ON DELETE SET NULL;
+ALTER TABLE "public".examination_information ADD CONSTRAINT fk_test_information FOREIGN KEY ( i_rate_of_difficulty_pk ) REFERENCES "public".rate_of_difficulty( i_rate_of_difficulty_pk );
 
-ALTER TABLE "public".examination_list ADD CONSTRAINT fk_examination_list_to_subject FOREIGN KEY ( i_subject_information_pk ) REFERENCES "public".subject_information( i_subject_information_pk );
+ALTER TABLE "public".examination_information ADD CONSTRAINT fk_examination_information_to_examination_room FOREIGN KEY ( i_examination_room_pk ) REFERENCES "public".examination_room( i_examination_room_pk ) ON DELETE SET NULL;
 
-ALTER TABLE "public".examination_list ADD CONSTRAINT fk_examination_list_to_teacher FOREIGN KEY ( i_teacher_information_pk ) REFERENCES "public".teacher_information( i_teacher_information_pk );
+ALTER TABLE "public".examination_information ADD CONSTRAINT fk_examination_information_to_subject FOREIGN KEY ( i_subject_information_pk ) REFERENCES "public".subject_information( i_subject_information_pk );
 
-ALTER TABLE "public".examination_list_detail ADD CONSTRAINT fk_examination_list_detail FOREIGN KEY ( i_examination_list_pk ) REFERENCES "public".examination_list( i_examination_list_pk ) ON DELETE SET NULL;
+ALTER TABLE "public".examination_information ADD CONSTRAINT fk_examination_information_to_teacher FOREIGN KEY ( i_teacher_information_pk ) REFERENCES "public".teacher_information( i_teacher_information_pk );
 
-ALTER TABLE "public".examination_result ADD CONSTRAINT fk_examination_result_to_student FOREIGN KEY ( i_student_information_pk ) REFERENCES "public".student_information( i_student_information_pk );
+ALTER TABLE "public".examination_information_detail ADD CONSTRAINT fk_examination_information_detail FOREIGN KEY ( i_examination_information_pk ) REFERENCES "public".examination_information( i_examination_information_pk ) ON DELETE SET NULL;
 
-ALTER TABLE "public".examination_result ADD CONSTRAINT fk_examination_result_to_test FOREIGN KEY ( i_test_information_pk ) REFERENCES "public".test_information( i_test_information_pk );
+ALTER TABLE "public".examination_information_detail ADD CONSTRAINT fk_examination_information_detail_to_student_information FOREIGN KEY ( i_student_information_pk ) REFERENCES "public".student_information( i_student_information_pk );
 
 ALTER TABLE "public".question_information ADD CONSTRAINT fk_question_information_to_teacher FOREIGN KEY ( i_teacher_information_pk ) REFERENCES "public".teacher_information( i_teacher_information_pk );
 
@@ -178,9 +180,10 @@ ALTER TABLE "public".student_information ADD CONSTRAINT fk_student_information F
 
 ALTER TABLE "public".teacher_information ADD CONSTRAINT fk_teacher_information FOREIGN KEY ( i_faculty_information_pk ) REFERENCES "public".faculty_information( i_faculty_information_pk );
 
-ALTER TABLE "public".test_detail_information ADD CONSTRAINT fk_test_detail_information_to_question FOREIGN KEY ( i_question_information_pk ) REFERENCES "public".question_information( i_question_information_pk );
+ALTER TABLE "public".test_information_detail ADD CONSTRAINT fk_test_detail_information_to_question FOREIGN KEY ( i_question_information_pk ) REFERENCES "public".question_information( i_question_information_pk );
 
-ALTER TABLE "public".test_detail_information ADD CONSTRAINT fk_test_detail_information_to_test FOREIGN KEY ( i_test_information_pk ) REFERENCES "public".test_information( i_test_information_pk );
+ALTER TABLE "public".test_information_detail ADD CONSTRAINT fk_test_detail_information_to_test FOREIGN KEY ( i_test_information_pk ) REFERENCES "public".test_information( i_test_information_pk );
 
-ALTER TABLE "public".test_information ADD CONSTRAINT fk_test_information FOREIGN KEY ( i_rate_of_difficulty_pk ) REFERENCES "public".rate_of_difficulty( i_rate_of_difficulty_pk );
+ALTER TABLE "public".test_information ADD CONSTRAINT fk_test_information FOREIGN KEY ( i_student_information_pk ) REFERENCES "public".student_information( i_student_information_pk );
 
+ALTER TABLE "public".test_information ADD CONSTRAINT fk_test_information_1 FOREIGN KEY ( i_examination_information_pk ) REFERENCES "public".examination_information( i_examination_information_pk );
